961cda229082e8c4f86033f7434610c59e25549b
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-02 16:24:50 +0900

    `Unbounded Thread Pool`을 활용하고자 로직을 수정했습니다. - `Executors.newCachedThreadPool()`을 활용해 Unbounded Thread Pool을 사용했습니다.

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -12,12 +12,11 @@ public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     private static final int PORT = 8080;
-    private static final int N_THREADS = 10;
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT); // 8080 포트에서 서버를 엽니다.
         log.debug("Listening for connection on port 8080 ....");
-        var executor = Executors.newFixedThreadPool(N_THREADS);
+        var executor = Executors.newCachedThreadPool();
         while (true) {
             executor.execute(new ConnectionHandler(serverSocket.accept()));
         }
