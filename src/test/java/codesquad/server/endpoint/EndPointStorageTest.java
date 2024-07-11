180cf4085af762e70b7a55b52d675c4bbed973f7
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-12 02:02:05 +0900

    누락된 테스트를 추가했습니다 - `EndPoint`, `EndPointStorage`

--- src/test/java/codesquad/server/endpoint/EndPointStorageTest.java
+++ src/test/java/codesquad/server/endpoint/EndPointStorageTest.java
@@ -1,46 +1,50 @@
 package codesquad.server.endpoint;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import codesquad.server.http.servlet.enums.HttpMethod;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class EndPointStorageTest {
 
-//    private EndPointStorage register;
-//
-//    @BeforeEach
-//    void setUp() {
-//        register = EndPointStorage.getInstance();
-//    }
-//
-//    @Test
-//    @DisplayName("Endpoint를 저장한다.")
-//    void testAddEndpoint() {
-//        // Arrange
-//        var expectedEndpoint = EndPoint.of("/index.html",
-//            (headers, value) -> HttpResponseDeprecated.from(StatusCode.OK));
-//        // Act
-//        register.addEndpoint(HttpMethod.GET, expectedEndpoint);
-//        // Assert
-//        assertThat(register.getEndpoint(HttpMethod.GET, "/index.html")).isEqualTo(expectedEndpoint);
-//    }
-//
-//    @Test
-//    @DisplayName("등록된 Endpoint를 반환한다.")
-//    void testGetEndpointSuccess() {
-//        // Arrange
-//        var expectedEndpoint = EndPoint.of("/index.html",
-//            (headers, value) -> HttpResponseDeprecated.from(StatusCode.OK));
-//        register.addEndpoint(HttpMethod.GET, expectedEndpoint);
-//        // Act
-//        var actualEndpoint = register.getEndpoint(HttpMethod.GET, "/index.html");
-//        // Assert
-//        assertThat(actualEndpoint).isEqualTo(expectedEndpoint);
-//    }
-//
-//    @Test
-//    @DisplayName("등록되지 않은 Endpoint를 요청하면 예외를 던진다.")
-//    void testGetEndpointFailWhenNotFound() {
-//        // Act & Assert
-//        assertThatThrownBy(() -> register.getEndpoint(HttpMethod.GET, "/index2.html"))
-//            .isInstanceOf(HttpCommonException.class)
-//            .hasFieldOrPropertyWithValue("statusCode", StatusCode.NOT_FOUND)
-//            .hasMessage("존재하지 않는 Endpoint 입니다.");
-//    }
+    private EndPointStorage storage;
+
+    @BeforeEach
+    void setUp() {
+        storage = EndPointStorage.getInstance();
+    }
+
+    @Test
+    @DisplayName("Endpoint를 저장한다.")
+    void testAddEndpoint() {
+        // Arrange
+        String expectedPath = "/";
+        EndPoint expectedEndpoint = EndPoint.of(expectedPath,
+            (httpServletRequest, httpServletResponse) -> {
+            });
+        // Act
+        storage.addEndpoint(HttpMethod.GET, expectedEndpoint);
+        // Assert
+        assertThat(storage.getEndpoint(HttpMethod.GET, "/")).isPresent().get()
+            .isEqualTo(expectedEndpoint);
+    }
+
+    @Test
+    @DisplayName("등록된 Endpoint를 반환한다.")
+    void testGetEndpointSuccess() {
+        // Arrange
+        String expectedPath = "/";
+        EndPoint expectedEndpoint = EndPoint.of(expectedPath,
+            (httpServletRequest, httpServletResponse) -> {
+            });
+        storage.addEndpoint(HttpMethod.GET, expectedEndpoint);
+        // Act
+        Optional<EndPoint> actualEndpoint = storage.getEndpoint(HttpMethod.GET, "/");
+        // Assert
+        assertThat(actualEndpoint).isPresent().get().isEqualTo(expectedEndpoint);
+    }
+
 }
--- src/test/java/codesquad/server/endpoint/EndPointTest.java
+++ src/test/java/codesquad/server/endpoint/EndPointTest.java
@@ -1,9 +1,94 @@
 package codesquad.server.endpoint;
 
-import org.junit.jupiter.api.Disabled;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Disabled
+import codesquad.server.http.servlet.HttpServletRequest;
+import codesquad.server.http.servlet.HttpServletResponse;
+import codesquad.server.http.servlet.enums.StatusCode;
+import java.util.function.BiConsumer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("EndPoint는")
 class EndPointTest {
 
+    @Nested
+    @DisplayName("생성 시")
+    class whenCreate {
+
+        @Test
+        @DisplayName("정상적으로 생성된다.")
+        void create() {
+            // Arrange
+            String path = "/index.html";
+            BiConsumer<HttpServletRequest, HttpServletResponse> biConsumer = (request, response) -> {
+            };
+            // Act
+            EndPoint actualResult = EndPoint.of(path, biConsumer);
+            // Assert
+            assertThat(actualResult).extracting("path", "biConsumer")
+                .containsExactly(path, biConsumer);
+        }
+
+        @Test
+        @DisplayName("BiConsumer가 null이면 예외를 던진다.")
+        void willThrowExceptionWhenBiConsumerIsNull() {
+            // Arrange
+            String path = "/index.html";
+            BiConsumer<HttpServletRequest, HttpServletResponse> biConsumer = null;
+            // Act & Assert
+            assertThatThrownBy(() -> EndPoint.of(path, biConsumer))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("BiConsumer는 null일 수 없습니다.");
+        }
+    }
+
+    @Test
+    @DisplayName("path가 같으면 같은 객체이다.")
+    void equals() {
+        // Arrange
+        String path = "/index.html";
+        BiConsumer<HttpServletRequest, HttpServletResponse> biConsumer = (request, response) -> {
+        };
+        EndPoint endPoint1 = EndPoint.of(path, biConsumer);
+        EndPoint endPoint2 = EndPoint.of(path, biConsumer);
+        // Act & Assert
+        assertThat(endPoint1).isEqualTo(endPoint2);
+    }
+
+    @Test
+    @DisplayName("path가 다르면 다른 객체이다.")
+    void notEquals() {
+        // Arrange
+        String path1 = "/index.html";
+        String path2 = "/index2.html";
+        BiConsumer<HttpServletRequest, HttpServletResponse> biConsumer = (request, response) -> {
+        };
+        EndPoint endPoint1 = EndPoint.of(path1, biConsumer);
+        EndPoint endPoint2 = EndPoint.of(path2, biConsumer);
+        // Act & Assert
+        assertThat(endPoint1).isNotEqualTo(endPoint2);
+    }
+
+    @Test
+    @DisplayName("apply 메서드를 실행하면 BiConsumer가 실행된다.")
+    void apply() {
+        // Arrange
+        String path = "/index.html";
+        HttpServletRequest request = new HttpServletRequest();
+        HttpServletResponse response = new HttpServletResponse();
+        BiConsumer<HttpServletRequest, HttpServletResponse> biConsumer = (httpServletRequest, httpServletResponse) -> {
+            request.setAttribute("key", "value");
+            response.setStatus(StatusCode.OK);
+        };
+        EndPoint endPoint = EndPoint.of(path, biConsumer);
+        // Act
+        endPoint.accept(request, response);
+        // Assert
+        assertThat(request.getAttribute("key")).isEqualTo("value");
+        assertThat(response.getStatus()).isPresent().get().isEqualTo(StatusCode.OK);
+    }
 
 }
