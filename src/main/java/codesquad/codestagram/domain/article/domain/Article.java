1da1e5a9960c9af17cd7f2745719331670f528c6
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-15 20:25:48 +0900

    Article 도메인 생성 및 DAO를 추가했습니다 - Article 도메인은 `title`과 `body`를 갖고 있습니다. - `ArticleDao`는 JDBC를 활용해 H2 Database에 정보를 저장 및 조회합니다.

---
+++ src/main/java/codesquad/codestagram/domain/article/domain/Article.java
@@ -0,0 +1,36 @@
+package codesquad.codestagram.domain.article.domain;
+
+public class Article {
+
+    private Long id;
+    private String title;
+    private String body;
+
+    public Article(String title, String body) {
+        this.title = validateTitle(title);
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    private String validateTitle(String title) {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("제목은 필수입니다.");
+        }
+        return title;
+    }
+}
---
+++ src/main/java/codesquad/codestagram/domain/article/storage/ArticleDao.java
@@ -0,0 +1,79 @@
+package codesquad.codestagram.domain.article.storage;
+
+import codesquad.codestagram.domain.article.domain.Article;
+import codesquad.server.database.H2ConnectManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArticleDao {
+
+    private static final Logger log = LoggerFactory.getLogger(ArticleDao.class);
+    private static final ArticleDao INSTANCE = new ArticleDao();
+
+    private final H2ConnectManager h2ConnectManager;
+
+    public ArticleDao() {
+        this.h2ConnectManager = H2ConnectManager.getInstance();
+    }
+
+    public static ArticleDao getInstance() {
+        return INSTANCE;
+    }
+
+    public void save(Article article) {
+        String insertSql = "INSERT INTO articles (title, body) VALUES (?, ?)";
+        try (Connection connection = h2ConnectManager.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(
+            insertSql)) {
+            preparedStatement.setString(1, article.getTitle());
+            preparedStatement.setString(2, article.getBody());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public Optional<Article> findById(Long id) {
+        String findByIdSql = "SELECT ARTICLES.ID, ARTICLES.TITLE, ARTICLES.BODY FROM articles WHERE id = ?";
+        try (Connection connection = h2ConnectManager.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(
+            findByIdSql)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                Article article = new Article(resultSet.getString("title"),
+                    resultSet.getString("body"));
+                article.setId(resultSet.getLong("id"));
+                return Optional.of(article);
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
+        return Optional.empty();
+    }
+
+    public List<Article> findAll() {
+        List<Article> articles = new ArrayList<>();
+        String findAllSql = "SELECT ARTICLES.ID, ARTICLES.TITLE, ARTICLES.BODY FROM articles";
+        try (Connection connection = h2ConnectManager.getConnection(); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(findAllSql);
+            while (resultSet.next()) {
+                Article article = new Article(resultSet.getString("title"),
+                    resultSet.getString("body"));
+                article.setId(resultSet.getLong("id"));
+                articles.add(article);
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
+        return Collections.unmodifiableList(articles);
+    }
+
+}
